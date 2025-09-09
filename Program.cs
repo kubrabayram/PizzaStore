54d0ccf0c26c16ccbfd8964cfc37f58039ebf309
Kübra  Bayram <kubrabayram@Kubra-MacBook-Pro.local>
2025-09-09 14:23:51 +0300

    root URL'den Swagger açılırhale getirildi.

--- Program.cs
+++ Program.cs
@@ -45,6 +45,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+         c.RoutePrefix = string.Empty; // root URL'den Swagger açılır
     });
 }
 
